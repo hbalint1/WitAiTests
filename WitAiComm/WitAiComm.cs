eeee5435424e20cfa9a4fa52038ececb1ef860d3
hbalint1 <hobalint94@gmail.com>
2017-10-22 21:21:58 +0200

    Added voice posting.

--- WitAiComm/WitAiComm.cs
+++ WitAiComm/WitAiComm.cs
@@ -1,11 +1,15 @@
-﻿using System.Net;
+﻿using Newtonsoft.Json;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Wit.Communication.WitAiComm
 {
     public static class WitAiComm
     {
         private const string _token = "RVA5TZYWEKPWREXZ6LQUJC2JZD4344ZH";
-        private const string _uri = "https://api.wit.ai/message?q=";
+        private const string _textUri = "https://api.wit.ai/message?q=";
+        private const string _voiceUri = "https://api.wit.ai/speech";
 
         public static Request SendRequest(string request)
         {
@@ -16,10 +20,33 @@ namespace Wit.Communication.WitAiComm
                 client.Headers[HttpRequestHeader.Authorization] = "Bearer " + _token;
 
                 // Make a request to the wit.ai server.
-                string result = client.DownloadString(_uri + request);
+                string result = client.DownloadString(_textUri + request);
 
                 return new Request { Search = request, Result = result };
             }
+        }
+
+        public async static Task<Request> PostVoiceAsync(byte[] data)
+        {
+            Request response = new Request();
+
+            await Task.Run(() =>
+            {
+                using (WebClient client = new WebClient())
+                {
+                    // Set up the header with the unique server access token.
+                    client.Headers.Add("Content-Type", "audio/wav");
+                    client.Headers[HttpRequestHeader.Authorization] = "Bearer " + _token;
+
+                    // Make a request to the wit.ai server.
+                    var result = client.UploadData(_voiceUri, data);
+                    var str = System.Text.Encoding.Default.GetString(result);
+                    WitAiResponse jsonResult = JsonConvert.DeserializeObject<WitAiResponse>(str);
+                    response = new Request { Search = jsonResult._text, Result = jsonResult.entities.intent.First().value };
+                }
+            });
+
+            return response;
         }
     }
 }
